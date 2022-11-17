593d2ad63207eafe5fd8f7f5f5f2f744f0170fc0
James Crowley <james.crowley@colorado.edu>
2022-11-16 20:21:39 -0700

    editied invert.sh

--- scripts/invert.sh
+++ scripts/invert.sh
@@ -13,14 +13,14 @@ module load gcc
 module load openmpi
 source /curc/sw/anaconda3/latest
 
-path_to_single_wrapper=$/projects/jacr0382/wrapper_1/
-path_to_atmos_wrapper=$/projects/jacr0382/wrapper_2/
-path_to_scripts=$/home/jacr0382/research_fall2022/scripts/
-path_to_data=$/scratch/alpine/jacr0382/HOP79/
-path_to_temp_data=$/scratch/alpine/jacr0382/temp/
+path_to_single_wrapper=/projects/jacr0382/wrapper_1/
+path_to_atmos_wrapper=/projects/jacr0382/wrapper_2/
+path_to_scripts=/home/jacr0382/research_fall2022/scripts/
+path_to_data=/scratch/alpine/jacr0382/HOP79/
+path_to_temp_data=/scratch/alpine/jacr0382/temp/
 
 # run start inversion python:
-current_data=$(python $path_to_scripts\start_inversion.py --path_to_assembled_fits $path_to_data)
+current_data=(python $path_to_scripts\start_inversion.py --path_to_assembled_fits $path_to_data)
 
 # iteration 0:
 mv $path_to_data\$current_data $path_to_temp_data\data.fits
@@ -36,8 +36,8 @@ python $path_to_atmos_wrapper\parallel_master_v0.51.py
 
 python $path_to_scripts\end_inversion.py --data_name $current_data \
                                          --path_to_SIR_output $path_to_atmos_wrapper\output \
-                                         --path_to_assembled_fits $path_to_data
-                                         --output_folder /scratch/alpine/jacr0382/HOP79_results/
+                                         --path_to_assembled_fits $path_to_data \
+                                         --output_folder /scratch/alpine/jacr0382/HOP79_results/ \
                                          --summary_file /home/jacr0382/inversion_summary_nov15.txt
 
 sbatch full_wrapper_2
